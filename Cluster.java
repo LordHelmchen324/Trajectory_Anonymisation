4b904337a7454a51df63216d79021ed29c990483
Jan Kaiser <jan.kaiser.email@googlmail.com>
2018-04-12 20:15:26 +0200

    Created class Cluster

---
+++ Cluster.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+class Cluster {
+
+    List<Trajectory> trajectories;
+
+    Cluster(List<Trajectory> trajectories) {
+        this.trajectories = trajectories;
+    }
+
+}
