aa62a10d773980211115628d575f38ebaa765358
Jan Kaiser <jan.kaiser.email@googlmail.com>
2018-04-14 16:30:23 +0200

    Got test to show up in VSCode and work from Terminal

--- TrajectoryTest.java
+++ TrajectoryTest.java
@@ -1,16 +1,28 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class TrajectoryTest {
 
-    void testAutocorrelation() {
+    @Test
+    public void autocorrelationAtZeroShouldAlwaysBeOne() {
         Trajectory r = new Trajectory();
         r.add(new Place(3, 6, 1));
         r.add(new Place(1, 7, 2));
         r.add(new Place(8, 5, 3));
 
-        String autocorr0Msg = "Testing autocorrelation at 0.0 == 1.0 ... ";
-        double autocorr0 = r.autocorrelation(0.0);
-        if (autocorr0 == 1.0) autocorr0Msg += "PASSED";
-        else autocorr0Msg += "FAILED\n  result = " + autocorr0;
-        System.out.println(autocorr0Msg);
+        Trajectory s = new Trajectory();
+        s.add(new Place(2, 9, 1));
+        s.add(new Place(7, 8, 2));
+        s.add(new Place(2, 4, 3));
+
+        Trajectory u = new Trajectory();
+        u.add(new Place(0, 3, 1));
+        u.add(new Place(6, 2, 2));
+        u.add(new Place(9, 2, 3));
+
+        assertEquals(1.0, r.autocorrelation(0.0), 0.001);
+        assertEquals(1.0, s.autocorrelation(0.0), 0.001);
+        assertEquals(1.0, u.autocorrelation(0.0), 0.001);
     }
 
 }
