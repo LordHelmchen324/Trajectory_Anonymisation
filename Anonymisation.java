394df20b148b8f314f145d812761e7da3f25d0be
Jan Kaiser <jan.kaiser.email@googlmail.com>
2018-04-15 03:05:42 +0200

    Cleaned up main method

--- Anonymisation.java
+++ Anonymisation.java
@@ -1,52 +1,7 @@
-import java.util.LinkedList;
-import java.util.List;
-
 class Anonymisation {
 
     public static void main(String[] args) {
-        /*TrajectoryTest trajTest = new TrajectoryTest();
-        trajTest.testAutocorrelation();*/
 
-        Trajectory r = new Trajectory();
-        r.add(new Place(3, 6, 1));
-        r.add(new Place(1, 7, 2));
-        r.add(new Place(8, 5, 3));
-
-        Trajectory s = new Trajectory();
-        s.add(new Place(2, 9, 1));
-        s.add(new Place(7, 8, 2));
-        s.add(new Place(2, 4, 3));
-
-        Trajectory u = new Trajectory();
-        u.add(new Place(0, 3, 1));
-        u.add(new Place(6, 2, 2));
-        u.add(new Place(9, 2, 3));
-    
-        /*double aeud = Trajectory.euclideanDistance(r, s);
-        System.out.println("Adapted Euclidean Distance r, s = " + aeud);
-    
-        double astsd = Trajectory.shortTimeSeriesDistance(r, s);
-        System.out.println("Adapted Short Time Series Distance r, s = " + astsd);
-
-        double autocorr0 = r.autocorrelation(0.0);
-        System.out.println("Adapted Autocorrelation r, 0.0 = " + autocorr0);
-        double autocorr1 = r.autocorrelation(1.0);
-        System.out.println("Adapted Autocorrelation r, 1.0 = " + autocorr1);*/
-
-        List<Trajectory> trajectories = new LinkedList<Trajectory>();
-        trajectories.add(r);
-        trajectories.add(s);
-        trajectories.add(u);
-
-        /*Cluster c1 = new Cluster(trajectories);
-        System.out.println("Before:\n" + c1.description());
-        c1.convertXMedianYMedian();
-        System.out.println("After:\n" + c1.description());*/
-
-        Cluster c2 = new Cluster(trajectories);
-        System.out.println("Before:\n" + c2.description());
-        c2.convertXMedianY();
-        System.out.println("After:\n" + c2.description());
     }
 
 }
