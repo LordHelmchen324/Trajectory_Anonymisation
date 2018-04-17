4c38940e5c6e2cb91dde601961ace00823fdb787
Jan Kaiser <jan.kaiser.email@googlmail.com>
2018-04-17 12:57:49 +0200

    Wrote main method to see what MDAV is doing

--- Anonymisation.java
+++ Anonymisation.java
@@ -1,7 +1,44 @@
 class Anonymisation {
 
     public static void main(String[] args) {
+        Dataset d = new Dataset();
 
+        Trajectory t1 = new Trajectory();
+        t1.add(new Place(1, 0, 0));
+        t1.add(new Place(3, 2, 4));
+        t1.add(new Place(4, 2, 3));
+        t1.add(new Place(5, 2, 3));
+        t1.add(new Place(5, 2, 3));
+        t1.add(new Place(9, 7, 7));
+        d.add(t1);
+
+        Trajectory t11 = new Trajectory();
+        t11.add(new Place(1, 0, 0));
+        t11.add(new Place(3, 2, 4));
+        t11.add(new Place(4, 2, 3));
+        t11.add(new Place(5, 2, 3));
+        t11.add(new Place(5, 2, 4));
+        t11.add(new Place(9, 7, 7));
+        d.add(t11);
+
+        Trajectory t2 = new Trajectory();
+        t2.add(new Place(1, 0, 0));
+        t2.add(new Place(9, 7, 7));
+        d.add(t2);
+        d.add(t2);
+
+        Trajectory t22 = new Trajectory();
+        t22.add(new Place(1, 0, 0));
+        t22.add(new Place(5, 3, 4));
+        t22.add(new Place(9, 7, 7));
+        d.add(t22);
+
+        DistanceMeasure dM = new ShortTimeSeriesDistance();
+        MedianStrategy mS = new XMedianY();
+
+        Dataset result = d.protectedByMDAV(2, dM, mS);
+
+        System.out.println("Orignal:\n" + d + "\n" + "Protected:\n" + result);
     }
 
 }
