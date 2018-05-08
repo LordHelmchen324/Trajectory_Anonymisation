020c8d12b301d300d7199d241909d323080faae2
Jan Kaiser <jan.kaiser.email@googlemail.com>
2018-05-08 14:03:46 +0200

    Improved plotting script for better looks

--- plot_ru_map.py
+++ plot_ru_map.py
@@ -12,8 +12,14 @@ for i in range(n) :
     il = float(sys.argv[1 + i * 2 + 1])
     information_losses.append(il)
 
+plt.style.use('ggplot')
+plt.figure(figsize = (8.5, 5))
 plt.axis((0, 100, 0, 100))
-plt.scatter(x = disclosure_risks, y = information_losses);
+plt.xticks([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+plt.yticks([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+plt.xlabel("Disclosure Risk")
+plt.ylabel("Information Loss")
+plt.scatter(x = disclosure_risks, y = information_losses, c = 'g', s = 80)
 plt.show()
 
 # python plot_ru_map.py 50.0 25.0 25.0 42.0
