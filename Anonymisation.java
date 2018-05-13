3c4fde3a07d4064cdc5ee91a1524346d3a2c4aa0
Jan Kaiser <jan.kaiser.email@googlemail.com>
2018-05-13 19:31:52 +0200

    Cleaned main method to only do anonymisation

--- Anonymisation.java
+++ Anonymisation.java
@@ -1,39 +1,14 @@
 class Anonymisation {
 
     public static void main(String[] args) {
-        Dataset d = Dataset.randomPerfect(1000, 6, 20, 20);
-        
-        // Assigning IDs to the Trajectories to find their protected version later
-        // TODO: Make sure IDs are unique
-        int i = 0;
-        for (Trajectory r : d.getTrajectories()) {
-            r.id = i;
-            i++;
-        }
-
-        System.out.println("Preparing DistanceMeasure and MedianStrategy ...");
+        Dataset d = Dataset.fromJSON("./randomPerfect1000.json");
 
         DistanceMeasure dM = new ShortTimeSeriesDistance();
         MedianStrategy mS = new XMedianY();
 
-        System.out.println("DistanceMeasure and MedianStrategy prepared.");
-
-        System.out.println("Protecting data set ...");
-
         Dataset result = d.protectedByMDAV(4, dM, mS);
 
-        System.out.println("Data set protection finished.\n");
-
-        System.out.println(result);
-
-        double dr = DisclosureRisk.computeViaRecordLinkage(d, result);
-        double il = InformationLoss.compute(d, result);
-
-        System.out.println("Disclosure risk: " + dr);
-        System.out.println("Information loss: " + il);
-        System.out.println("");
-
-        Plotter.plotRUMap(dr, il);
+        Dataset.toJSON(result, "./randomPerfect1000_Result.json");
     }
 
 }
