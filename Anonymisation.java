9a360d52c5e34d9f3eb2d3469c06df6c5baa8f40
Jan Kaiser <jan.kaiser.email@googlemail.com>
2018-06-18 20:01:16 +0200

    Added path to perfect data set

--- Anonymisation.java
+++ Anonymisation.java
@@ -5,7 +5,8 @@ class Anonymisation {
 
     public static void main(String[] args) {
         //Dataset d = Dataset.fromJSON("../Geolife Trajectories 1.3/MS_GeoLife_pandas-downsampled.json");
-        /*Dataset d = Dataset.fromJSON("../San Francisco Taxi Data/CabSpotting_pandas-downsampled.json");
+        //Dataset d = Dataset.fromJSON("../San Francisco Taxi Data/CabSpotting_pandas-downsampled.json");
+        Dataset d = Dataset.fromJSON("../Perfect Data Set/perfect_grid_1000_60_2000_2000.json");
 
         DistanceMeasure dM = new SynchronisedDistance();
         List<Dataset> ds = new LinkedList<Dataset>(); ds.add(d);
@@ -20,13 +21,11 @@ class Anonymisation {
 
             //Dataset.toJSON(result, "../Geolife Trajectories 1.3/MS_GeoLife_pandas-downsampled_PROTECTED.json");
             //Dataset.toCSV(result, "../Geolife Trajectories 1.3/MS_GeoLife_pandas-downsampled_PROTECTED.csv");
-            Dataset.toJSON(result, "../San Francisco Taxi Data/CabSpotting_pandas-downsampled_PROTECTED_k" + k + ".json");
-            Dataset.toCSV(result, "../San Francisco Taxi Data/CabSpotting_pandas-downsampled_PROTECTED_k" + k + ".csv");
-        }*/
-        
-        Dataset d = Dataset.randomPerfectOnGrid(1000, 60, 2000, 2000);
-        Dataset.toJSON(d, "../Perfect Data Set/perfect_grid_1000_60_2000_2000.json");
-        Dataset.toCSV(d, "../Perfect Data Set/perfect_grid_1000_60_2000_2000.csv");
+            //Dataset.toJSON(result, "../San Francisco Taxi Data/CabSpotting_pandas-downsampled_PROTECTED_k" + k + ".json");
+            //Dataset.toCSV(result, "../San Francisco Taxi Data/CabSpotting_pandas-downsampled_PROTECTED_k" + k + ".csv");
+            Dataset.toJSON(d, "../Perfect Data Set/perfect_grid_1000_60_2000_2000_PROTECTED_k" + k + ".json");
+            Dataset.toCSV(d, "../Perfect Data Set/perfect_grid_1000_60_2000_2000_PROTECTED_k" + k + ".csv");
+        }
     }
 
 }
